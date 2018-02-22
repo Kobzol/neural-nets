0c0f5313747525841591544cdf33e6267b67bba1
Jakub Beránek <berykubik@gmail.com>
2018-02-22 20:25:16 +0100

    ENH: add bia subproject and and basic neuroevolution using DE

---
+++ src/main/kotlin/main/neuroevolution/main.kt
@@ -0,0 +1,82 @@
+package main.neuroevolution
+
+import algorithm.Bounds
+import algorithm.FitnessEvaluator
+import algorithm.PopulationGenerator
+import algorithm.evolution.crossover.DECrossover
+import algorithm.evolution.de.DE
+import algorithm.evolution.mutation.DERand1
+import koma.extensions.set
+import nn.NetBuilder
+import nn.activation.Sigmoid
+import nn.createNormalInitializer
+import nn.layer.Perceptron
+import nn.toVec
+
+fun main(args: Array<String>)
+{
+    val net = NetBuilder()
+            .add { s -> Perceptron(s, 2, Sigmoid(), createNormalInitializer()) }
+            .add { s -> Perceptron(s, 1, Sigmoid(), createNormalInitializer()) }
+            .build(2)
+    val inputs = listOf(
+            floatArrayOf(0.0f, 0.0f),
+            floatArrayOf(0.0f, 1.0f),
+            floatArrayOf(1.0f, 0.0f),
+            floatArrayOf(1.0f, 1.0f)
+    ).map { toVec(it) }
+    val outputs = listOf(
+            floatArrayOf(0.0f),
+            floatArrayOf(1.0f),
+            floatArrayOf(1.0f),
+            floatArrayOf(0.0f)
+    ).map { toVec(it) }
+
+    val bounds = Bounds(-1.0f, 1.0f)
+    val boundsArray = arrayOf(
+            bounds,
+            bounds,
+            bounds,
+            bounds,
+            bounds,
+            bounds,
+            bounds,
+            bounds,
+            bounds
+    )
+
+    var best = 1.0f
+    val evaluator =  object: FitnessEvaluator {
+        override fun evaluate(data: FloatArray): Float
+        {
+            net.layers[0].biases[0] = data[0]
+            net.layers[0].weights[0, 0] = data[1]
+            net.layers[0].weights[0, 1] = data[2]
+            net.layers[0].biases[1] = data[3]
+            net.layers[0].weights[1, 0] = data[4]
+            net.layers[0].weights[1, 1] = data[5]
+            net.layers[1].biases[0] = data[6]
+            net.layers[1].weights[0, 0] = data[7]
+            net.layers[1].weights[0, 1] = data[8]
+
+            val loss = net.getLoss(inputs, outputs)
+            best = Math.min(best, loss)
+            println(loss)
+
+            return 1.0f - loss
+        }
+    }
+    val de = DE(PopulationGenerator().generateAreaPopulation(100, boundsArray),
+            DECrossover(), 0.5f,
+            DERand1(boundsArray), 0.2f,
+            boundsArray, evaluator
+    )
+
+
+    for (i in 0 until 5000)
+    {
+        de.runIteration()
+    }
+
+    println(best)
+}
